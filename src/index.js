88ef4f5e62836ac14900df9cab6ef862d02decdb
Silvana <silvana15_91@hotmail.com>
2019-07-23 17:09:21 -0500

    se agrego boton de face

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@
 <!-- TODO: Add SDKs for Firebase products that you want to use
      https://firebase.google.com/docs/web/setup#config-web-app -->
 
+
   // Your web app's Firebase configuration
   var firebaseConfig = {
     apiKey: "AIzaSyDF1XiDAijYoqmcfmzKIs9EAb2DkOHIrY8",
@@ -17,25 +18,8 @@
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
-<!-- The core Firebase JS SDK is always required and must be listed first -->
-<script src="https://www.gstatic.com/firebasejs/6.3.1/firebase-app.js"></script>
-
-<!-- TODO: Add SDKs for Firebase products that you want to use
-     https://firebase.google.com/docs/web/setup#config-web-app -->
-
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyDF1XiDAijYoqmcfmzKIs9EAb2DkOHIrY8",
-    authDomain: "red-sorora-7c6fc.firebaseapp.com",
-    databaseURL: "https://red-sorora-7c6fc.firebaseio.com",
-    projectId: "red-sorora-7c6fc",
-    storageBucket: "",
-    messagingSenderId: "210204876902",
-    appId: "1:210204876902:web:739809e95c8f138c"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig); window.fbAsyncInit = function() {
-        FB.init({
+  window.fbAsyncInit = function() {
+    FB.init({
       appId      : '{498063954299569}',
       cookie     : true,
       xfbml      : true,
@@ -53,3 +37,5 @@
      js.src = "https://connect.facebook.net/en_US/sdk.js";
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));
+
+
