e307c2ab931f7648645964b7286ebaf46b641f5d
Kaviza90 <vieralaqueviera@gmail.com>
2019-07-24 18:52:58 -0500

    se modifica html y js

--- src/index.js
+++ src/index.js
@@ -7,53 +7,59 @@ var btn = document.getElementById("register_user");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal 
+// When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
-}
+};
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
-}
+};
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
+};
 //Add event listener to the register button on modal
-let register_button = document.getElementById('register_button');
-register_button.addEventListener('click',handleSignUp);
-
+let register_button = document.getElementById("register_button");
+register_button.addEventListener("click", handleSignUp);
 
 /**
-  * Handles the sign up button press.
-*/
+ * Handles the sign up button press.
+ */
 function handleSignUp() {
-  var email = document.getElementById('register_email').value;
-  var password = document.getElementById('register_password').value;
+  var email = document.getElementById("register_email").value;
+  var password = document.getElementById("register_password").value;
   if (email.length < 4) {
-    alert('Please enter an email address.');
+    alert("Please enter an email address.");
     return;
   }
   if (password.length < 4) {
-    alert('Please enter a password.');
+    alert("Please enter a password.");
     return;
-  }  
+  }
   //Para acceder con una ventana emergente, llama a signInWithPopup
-  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ...
-  });
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ...
+    });
 }
 
 //Para cerrar la sesión de un usuario, llama a signOut de la siguiente manera
-firebase.auth().signOut().then(function() {
-  // Sign-out successful.
-}).catch(function(error) {
-  // An error happened.
-});
+firebase
+  .auth()
+  .signOut()
+  .then(function() {
+    // Sign-out successful.
+  })
+  .catch(function(error) {
+    // An error happened.
+  });
