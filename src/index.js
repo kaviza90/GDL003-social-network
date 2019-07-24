89513c176664d5fb4725a514646744b48334a615
carmenlab <cavagml@gmail.com>
2019-07-24 14:14:45 -0500

    Se elimino repeticion de configuracion

--- src/index.js
+++ src/index.js
@@ -17,22 +17,7 @@
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
-<!-- The core Firebase JS SDK is always required and must be listed first -->
-<script src="https://www.gstatic.com/firebasejs/6.3.1/firebase-app.js"></script>
-
-<!-- TODO: Add SDKs for Firebase products that you want to use
-     https://firebase.google.com/docs/web/setup#config-web-app -->
-
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyDF1XiDAijYoqmcfmzKIs9EAb2DkOHIrY8",
-    authDomain: "red-sorora-7c6fc.firebaseapp.com",
-    databaseURL: "https://red-sorora-7c6fc.firebaseio.com",
-    projectId: "red-sorora-7c6fc",
-    storageBucket: "",
-    messagingSenderId: "210204876902",
-    appId: "1:210204876902:web:739809e95c8f138c"
-  };
+  
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig); window.fbAsyncInit = function() {
         FB.init({
