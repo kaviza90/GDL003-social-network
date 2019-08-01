136d177627a73b35916e4d9390cf4153de4cfe55
carmenlab <cavagml@gmail.com>
2019-07-31 23:19:24 -0500

    se quito el form

--- src/index.js
+++ src/index.js
@@ -32,65 +32,55 @@ var txtPassword = document.getElementById("userPassword");
 var btnLogin = document.getElementById("login");
 var btnLogout = document.getElementById("logout");
 
-//var auth = firebase.auth();
-
+var auth = firebase.auth();
 
 //Function Login
 const loginUser = () => {
   var email = txtEmail.value;
-  var password = txtPassword.value;
-  firebase.auth().signInWithEmailAndPassword(email, password)
-  .then(function() {
+  var pass = txtPassword.value;
+  auth.signInWithEmailAndPassword(email, pass)
+  .then(() => {
     console.log("sesion iniciada");
   })
-  .catch(function(error) {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    //console.log("Necesitas Registrarte primero");
+  .catch( error => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log("Necesitas Registrarte primero");
     console.log(errorMessage);
     console.log(errorCode);
   });
 }
 
 //Login
-btnLogin.addEventListener("click",()=> {loginUser()});
+btnLogin.addEventListener("click", loginUser);
 
 /* Register if the user is new.*/
 const RegisterNew = () => {
   var email = txtEmail.value;
   var password = txtPassword.value;
-  firebase.auth().createUserWithEmailAndPassword(email, password)
-  .then(function() {
+  auth.createUserWithEmailAndPassword(email, password)
+  .then(() => {
     console.log("Registro exitoso");
   })
-  .catch( function (error) {
-    var errorCode = error.code;
-    var errorMessage = error.message;
+  .catch( error => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
     console.log(errorMessage);
     console.log(errorCode);
-  });
+  })
 }
 
-btnLogin.addEventListener("click",()=> {RegisterNew()});
-//btnRegister.addEventListener("click", RegisterNew);
+btnRegister.addEventListener("click", RegisterNew);
 
 //Logout Cerrar sesión de un usuario
 const logoutUser = () => {
-  firebase.auth().signOut()
-  .then(function() {
-    // Sign-out successful.
-  })
-  .catch(function(error) {
-    // An error happened.
-  });
-}
+  auth.signOut();
+};
 
-
-//btnLogout.addEventListener("click", logoutUser);
-btnLogin.addEventListener("click",()=> {logoutUser()});
+btnLogout.addEventListener("click", logoutUser);
 
 //Detectar si ya esta Logeado
-/*firebase.auth().onAuthStateChanged( user =>{
+auth.onAuthStateChanged(user =>{
   if (user){
     console.log(user);
     console.log("Ha sido registrado exitosamente");
@@ -99,5 +89,4 @@ btnLogin.addEventListener("click",()=> {logoutUser()});
     console.log("No estas Registrado");
     btnLogout.style.display = "none";
   }
-})
-*/
+});
