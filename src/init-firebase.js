86b43b536607f886fd890db8c71f3b981c9be744
carmenlab <cavagml@gmail.com>
2019-07-31 23:37:27 -0500

    Se arreglo conflicto inifirebase

--- src/init-firebase.js
+++ src/init-firebase.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyDXf5GUypFtGvOc_7wXygYtsP5QT-MgKxc",
-    authDomain: "redsorora-f7a37.firebaseapp.com",
-    databaseURL: "https://redsorora-f7a37.firebaseio.com",
-    projectId: "redsorora-f7a37",
-    storageBucket: "",
-    messagingSenderId: "175656874379",
-    appId: "1:175656874379:web:9b1f476186f7d82f"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  
-=======
 // Your web app's Firebase configuration
  var firebaseConfig = {
    apiKey: "AIzaSyDXf5GUypFtGvOc_7wXygYtsP5QT-MgKxc",
@@ -26,4 +11,3 @@
  // Initialize Firebase
  firebase.initializeApp(firebaseConfig);
 
->>>>>>> 82ecdf60a3f620c242fb76a2c385f6a3fe02f722
